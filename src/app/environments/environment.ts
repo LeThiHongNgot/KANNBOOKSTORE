be68cea1e1fe92920a5f87fc915b90b72aed6067
LeThiHongNgot <lehongngot17102003@gmail.com>
2024-08-03 01:10:35 +0700

    t

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,7 +1,7 @@
 
 export const environment = {
   production: false,
-  baseUrl:"https://localhost:7009/api/",
+  baseUrl:"kannbookstore.azurewebsites.net/api/",
   keywords: 'sách, mua sách trực tuyến, sách văn học, sách tiểu thuyết, sách mới nhất, giảm giá sách, bookstore, KANN',
   firebaseConfig :
   {
