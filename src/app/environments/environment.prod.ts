3fa1675937e12c10a4b60fdc4e299e5edee7312f
LeThiHongNgot <lehongngot17102003@gmail.com>
2024-08-11 18:05:14 +0700

    first commit

---
+++ src/app/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: true,
+  //baseUrl: "https://quanlycuahangsach.azurewebsites.net/api",
+  baseUrl: "https://localhost:7009/api/",
+  firebaseConfig:
+  {
+    apiKey: "AIzaSyB8K_ggTJEaAMtANChnFkB1TuexTE4ysnY",
+    authDomain: "phone-auth-6da2a.firebaseapp.com",
+    projectId: "phone-auth-6da2a",
+    storageBucket: "phone-auth-6da2a.appspot.com",
+    messagingSenderId: "68070531758",
+    appId: "1:68070531758:web:d81cb5be978ad787adc33f"
+  }
+};
